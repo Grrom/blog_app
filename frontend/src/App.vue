f1a73b9fdc15017c36dcd0a466ef9ef0f6c36a5c
Grrom <josejeromelalunio@gmail.com>
2023-01-27 09:53:07 +0800

    creat blog dialog

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,54 +7,58 @@
     >
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          src="@/assets/blog.svg"
           transition="scale-transition"
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <v-banner text="Blog App">
+          Blog App
+        </v-banner>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      <!-- <v-btn
+        v-on:click="test"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <span class="mr-2">Create Blog</span>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn> -->
+      <CreateBlogForm/>
     </v-app-bar>
 
+
     <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
+import CreateBlogForm from "./components/CreateBlogForm"
+// import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    CreateBlogForm,
+    // HelloWorld
   },
 
   data: () => ({
-    //
+    showAlert: false
   }),
+  methods:{
+    test: function(){
+      this.showAlert=!this.showAlert
+    }
+  }
 };
 </script>
